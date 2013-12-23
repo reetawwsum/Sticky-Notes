df934fa5f342a23db059525ad92e61e657f6e57e
reetawwsum <reetawwsum@yahoo.com>
2013-12-23 23:16:46 +0530

    Added feature of storage with lock/unlock option

--- js/jqController.js
+++ js/jqController.js
@@ -1,6 +1,6 @@
+var lock = 0;
 //JQuery Controller
 $(document).ready(function(){
-	var lock = 0;
 	//Note Close
 	$("#cross").click(function(){
 		var appWindow = chrome.app.window.current();
@@ -10,9 +10,11 @@ $(document).ready(function(){
 	$("#lock").click(function(){
 		if (!lock){
 			$(this).attr("src", "assets/lock.png");
+			$("textarea").attr("readonly", true);
 			lock = 1;
 		} else {
 			$(this).attr("src", "assets/unlock.png");
+			$("textarea").attr("readonly", false);
 			lock = 0;
 		}
 	});
--- js/ngController.js
+++ js/ngController.js
@@ -1,4 +1,23 @@
 //AngularJs Controller
 function MainController($scope){
-	
+	chrome.storage.sync.get('note', function(value){
+		$scope.$apply(function(){
+			$scope.load(value);
+		});
+	});
+
+	$scope.load = function(value){
+		if (value && value.note && value.note != ""){
+			$scope.note = value.note;
+			$("#lock").click();
+		}
+	}
+
+	$scope.save = function(){
+		if (!lock){
+			chrome.storage.sync.set({'note': $scope.note});
+		} else {
+			chrome.storage.sync.set({'note': ""});
+		}
+	}
 }
